893a5fa709cdabbe787af9d97aba93154865ba35
eric.voots@gmail.com <eric.voots@gmail.com>
2020-12-15 17:43:05 -0500

    first

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+
+def main_text_test(string):
+    '''
+
+    :param string:
+    :return:
+    '''
+
+    print(string)
+
+    # init master dict
+    master_string_dict = {}
+
+    # inner string dict
+    inner_string = {}
+
+    return master_string_dict
+
+
+if __name__ == "__main__":
+
+    test_string = 'The first number is 1111 but the number 2222 is not far behind'
+
+    main_text_test(test_string)
